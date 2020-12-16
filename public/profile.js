0a326b3fbc70b596d165985f61d232a08c5266e3
Tomer Ayal <tomer.trc@gmail.com>
2020-12-16 11:07:14 +0200

    Added display name to user

--- public/profile.js
+++ public/profile.js
@@ -6,11 +6,35 @@ function logOut() {
     });
 }
 
+function updateProfile(e) {
+    e.preventDefault();
+
+    const elements = document.getElementById("updateProfile").elements;
+    const name = elements.namedItem("name").value;
+    let newUserData = {};
+
+    if (name) {
+        newUserData.displayName = name;
+    }
+
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile(newUserData).then(() => {
+        document.getElementById('updateProfileText').innerText = "Successfully updated profile";
+    }).catch((e) => {
+        document.getElementById('updateProfileText').innerText = e.message;
+    })
+
+    location.reload();
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        document.getElementById('greetingText').innerText += user.email;
+        document.getElementById('greetingText').innerText += user.displayName + '!';
         document.getElementById('greeting').style.display = "block";
     } else {
         location.href = '/';
     }
 })
+
+document.getElementById("updateProfile").onsubmit = updateProfile;
--- public/sign-up.js
+++ public/sign-up.js
@@ -1,15 +1,31 @@
 function signUp(e) {
     e.preventDefault();
+    const errorMessage = document.getElementById('error');
     const elements = document.getElementById("signupForm").elements;
     const email = elements.namedItem("email").value;
     const password = elements.namedItem("password").value;
+    const name = elements.namedItem("name").value;
+
+    if (!email) {
+        errorMessage.innerHTML = "Please enter a valid email address";
+        return;
+    }
+    if (!password) {
+        errorMessage.innerHTML = "Please enter a password";
+    }
+    if (!name) {
+        errorMessage.innerHTML = "Please enter a display name";
+    }
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((user) => {
+        user.updateProfile({
+            displayName: name,
+        })
         location.href = '/';
     })
     .catch((error) => {
-        document.getElementById('error').innerHTML = error.message;
+        errorMessage.innerHTML = error.message;
     });
 }
 

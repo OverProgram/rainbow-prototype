b307eb15399b1f9dd9c84c31dd5008ee78ab981d
Tomer Ayal <tomer.trc@gmail.com>
2020-12-16 11:30:06 +0200

    added password reset

---
+++ public/forgot-password.js
@@ -0,0 +1,13 @@
+function resetPassword(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("forgotPasswordForm").elements.namedItem("email").value;
+
+    firebase.auth().sendPasswordResetEmail(email).then(() => {
+        document.getElementById("message").innerHTML = "Email sent!";
+    }).catch((error) => {
+        document.getElementById("message").innerHTML = error.message;
+    })
+}
+
+document.getElementById("forgotPasswordForm").onsubmit = resetPassword;

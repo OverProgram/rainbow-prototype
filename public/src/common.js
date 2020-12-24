cb20a9b46d226695e5ea65c2d6ab9b8f4dcb841c
Tomer Ayal <tomer.trc@gmail.com>
2020-12-24 21:58:43 +0200

    moved scripts to src folder

---
+++ public/src/common.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDSu-lsbMtD2vtNeBBzk1ECod0Bl1eDyOU",
+    authDomain: "rainbow-test-f98ac.firebaseapp.com",
+    databaseURL: "https://rainbow-test-f98ac-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "rainbow-test-f98ac",
+    storageBucket: "rainbow-test-f98ac.appspot.com",
+    messagingSenderId: "630598121962",
+    appId: "1:630598121962:web:a9278a12b69c17f3179a93",
+    measurementId: "G-W0GJXVR65G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
